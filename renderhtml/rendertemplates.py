f41b4fbf5def591b000fd51e3db1ec95fce8ddb3
BKDev <madautodev@gmail.com>
2022-09-02 00:04:18 +1000

    initial commit

---
+++ renderhtml/rendertemplates.py
@@ -0,0 +1,11 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+class RenderHTML:
+    def __init__(self, template_dir: str) -> None:
+        self.template_dir = template_dir
+        self.env = Environment(loader=FileSystemLoader(self.template_dir))
+
+    def render_template(self, filename: str, **kwargs) -> str:
+        """Returns a html template replacing placeholders with key word args"""
+        return self.env.get_template(filename).render(kwargs)
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(cwd, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "0.0.1"
+DESCRIPTION = "For rendering minified html output built dynamically from templates using jinja2 syntax"
+
+setup(
+    name="renderhtml",
+    version=VERSION,
+    author="Dan Hobday",
+    author_email="<danhobday@proton.me>",
+    url="https://github.com/DPHobday/renderhtml",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["jinja2"],
+    keywords=["python", "html"],
+    classifiers=[
+        "Development Status :: Beta",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
